62b690471d796eadaa83bed51b6087c4845632c9
Anchal-T <anchal2004tandekar@gmail.com>
2025-03-13 10:45:52 +0000

    added game state management and initialization functions

--- minswepper.cpp
+++ minswepper.cpp
@@ -21,6 +21,15 @@ typedef struct Cell
 	bool flag;
 } Cell;
 
+typedef enum state
+{	
+	PLAYING,
+	WIN,
+	LOSE
+}GameState;
+
+GameState state;
+
 Cell grid[ROWS][COLS];
 
 void DrawCells(Cell);
@@ -29,9 +38,15 @@ void IsRevealed(int, int);
 int countMines(int, int);
 void IsCellFlagged(int, int);
 void ClearGrid(int, int);
+void game_init();
+void RevealAllMines();
+void CheckWinCondition();
 
 Texture2D flagTexture;
 Texture2D bombTexture;
+
+float gameStartTime;
+float gameEndTime;
 
 int main(int argc, char const *argv[])
 {
@@ -46,40 +61,7 @@ int main(int argc, char const *argv[])
 	bombTexture = LoadTextureFromImage(bombImage);
 	UnloadImage(bombImage);
 
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			grid[i][j] = (Cell){
-				.i = i,
-				.j = j,
-				.containsMine = false,
-				.revealed = false,
-				.MinesNearby = -1,
-				.flag = false};
-		}
-	}
-
-	int mineToPlace = (int)(ROWS * COLS * 0.1f);
-	while (mineToPlace > 0)
-	{
-		int i = rand() % COLS;
-		int j = rand() % ROWS;
-
-		if (!grid[i][j].containsMine)
-		{
-			grid[i][j].containsMine = true;
-			mineToPlace--;
-		}
-	}
-
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			grid[i][j].MinesNearby = countMines(i, j);
-		}
-	}
+	game_init();
 
 	while (!WindowShouldClose())
 	{
@@ -105,8 +87,13 @@ int main(int argc, char const *argv[])
 				IsCellFlagged(IndexI, IndexJ);
 			}
 		}
+		else if(IsKeyPressed(KEY_R)){
+			game_init();
+		}
+		
 		BeginDrawing();
 		ClearBackground(WHITE);
+		
 		for (int i = 0; i < ROWS; ++i)
 		{
 			for (int j = 0; j < COLS; ++j)
@@ -114,6 +101,27 @@ int main(int argc, char const *argv[])
 				DrawCells(grid[i][j]);
 			}
 		}
+		
+		if(state == PLAYING) {
+			CheckWinCondition();
+		}
+		
+		if(state == LOSE){
+			DrawRectangle(screenWidth/2 - 120, screenHeight/2 - 30, 240, 60, Color{0, 0, 0, 180});
+			DrawText("Game Over", screenWidth/2 - 100, screenHeight/2 - 20, 40, RED);
+		} else if(state == WIN) {
+			DrawRectangle(screenWidth/2 - 100, screenHeight/2 - 30, 200, 60, Color{0, 0, 0, 180});
+			DrawText("You Win!", screenWidth/2 - 80, screenHeight/2 - 20, 40, GREEN);
+		}
+		
+		if(state == PLAYING) {
+			float currentTime = GetTime() - gameStartTime;
+			DrawText(TextFormat("Time: %.1f", currentTime), 10, 10, 20, BLACK);
+		} else {
+			float finalTime = gameEndTime - gameStartTime;
+			DrawText(TextFormat("Final Time: %.1f", finalTime), 10, 10, 20, BLACK);
+		}
+		
 		EndDrawing();
 	}
 
@@ -122,7 +130,8 @@ int main(int argc, char const *argv[])
 }
 
 void DrawCells(Cell cell)
-{
+{	
+		
 	if (cell.revealed)
 	{
 		if (cell.containsMine)
@@ -165,11 +174,17 @@ bool IsValid(int I, int J)
 
 void IsRevealed(int I, int J)
 {
-	if (grid[I][J].flag || grid[I][J].revealed)
+	if (grid[I][J].flag || grid[I][J].revealed || state != PLAYING)
 	{
 		return;
 	}
 	grid[I][J].revealed = true;
+
+	if(grid[I][J].containsMine){
+		state = LOSE;
+		gameEndTime = GetTime();
+		RevealAllMines();
+	}
 
 	if(grid[I][J].MinesNearby == 0){
 		ClearGrid(I, J);
@@ -214,7 +229,7 @@ void IsCellFlagged(int i, int j)
 void ClearGrid(int i, int j){
 	for (int I = -1; I <= 1; I++)
 	{
-		for (int J = -1; J <= 1; J++)
+		for (int J = -1; I <= 1; J++)
 		{
 			if (I == 0 && J == 0)
 			{
@@ -227,4 +242,84 @@ void ClearGrid(int i, int j){
 			IsRevealed(i + I, j + J);
 		}
 	}
+}
+
+void RevealAllMines()
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (grid[i][j].containsMine) {
+				grid[i][j].revealed = true;
+			}
+		}
+	}
+}
+
+void CheckWinCondition()
+{
+	int totalCells = ROWS * COLS;
+	int revealedCells = 0;
+	int mineCount = 0;
+	
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (grid[i][j].containsMine) {
+				mineCount++;
+			}
+			if (grid[i][j].revealed) {
+				revealedCells++;
+			}
+		}
+	}
+	
+	if (revealedCells + mineCount == totalCells) {
+		state = WIN;
+		gameEndTime = GetTime();
+	}
+}
+
+void game_init(){
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			grid[i][j] = (Cell){
+				.i = i,
+				.j = j,
+				.containsMine = false,
+				.revealed = false,
+				.MinesNearby = -1,
+				.flag = false
+			};
+		}
+	}
+	state = PLAYING;
+	gameStartTime = GetTime();
+	gameEndTime = 0;
+	
+	int mineToPlace = (int)(ROWS * COLS * 0.1f);
+	while (mineToPlace > 0)
+	{
+		int i = rand() % COLS;
+		int j = rand() % ROWS;
+
+		if (!grid[i][j].containsMine)
+		{
+			grid[i][j].containsMine = true;
+			mineToPlace--;
+		}
+	}
+
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			grid[i][j].MinesNearby = countMines(i, j);
+		}
+	}
+
 }
